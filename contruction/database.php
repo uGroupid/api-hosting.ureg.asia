06f1e3315f1b8ec69e47f1a1e74c1c7a5a2b8369
uGroupid <ugroupid>
2018-01-18 12:40:57 +0700

    add readme contruct

--- contruction/database.php
+++ contruction/database.php
@@ -1,16 +1,44 @@
 <?php 
 class Database_contruct {
-    public function cfg_role_server(
+    public function cfg_role_server(){
         $params = array(
             array('name_role_server' => 'Linux Centos [ISP Control Panel]',),
             array('name_role_server' => 'Java [BlueOneyx Control Panel]',),
             array('name_role_server' => 'Windown 2008 [Plesk Control Panel]',),
         );
         return json_encode($params,true);
-    );
+    }
 
-    public function host_server();
-    public function host_service();
-    public function host_package();
+    public function host_cfg_server(){
+        $params = array(
+            array(
+                array(
+                    'label_server' => 'VPS1',
+                    'name_server' => '192.168.1.221',
+                    'ip_server' => '192.168.1.221',
+                    'username' => '192.168.1.221',
+                    'password' => '192.168.1.221',
+                    'soap_uri' => '192.168.1.221',
+                    'soap_location' => '192.168.1.221',
+                    'role_server' => '192.168.1.221',
+                ),
+            ),  
+        );
+        return json_encode($params,true);
+    }
+    public function host_service(){
+        $params = array(
+            array(
+                'NameService',
+                'Price',
+                'đ/tháng',
+                'Discount',
+                'SaleOff',
+            ),
+        );
+    }
+    public function host_package(){
+
+    }
 }
 ?>
