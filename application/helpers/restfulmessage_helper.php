c74854e3e259b2248b6d40cdd67cf9aa889eed98
uGroupid <ugroupid>
2018-01-26 18:47:48 +0700

    edit api

--- application/helpers/restfulmessage_helper.php
+++ application/helpers/restfulmessage_helper.php
@@ -5,17 +5,56 @@ if(! function_exists('api_message')){
 			case 00:
 				return array(
 					'code' => 00,
-					'msg' => 'Thành công',
+					'msg' => 'Command Successful Transaction',
 				);
 				break;
 			case 99:
 				return array(
 					'code' => 99,
-					'msg' => 'Thất bại',
+					'msg' => 'Command Transaction Failed',
 				);
 				break;
-			case 2:
-				echo "i equals 2";
+			case 01:
+				return array(
+					'code' => 01,
+					'msg' => 'Command Syntax Error',
+				);
+				break;
+			case 02:
+				return array(
+					'code' => 02,
+					'msg' => 'Token Syntax Error',
+				);
+				break;
+			case 03:
+				return array(
+					'code' => 03,
+					'msg' => 'Secret do not exist',
+				);
+				break;
+			case 04:
+				return array(
+					'code' => 04,
+					'msg' => 'keys do not exist',
+				);
+				break;
+			case 05:
+				return array(
+					'code' => 05,
+					'msg' => 'Token do not exist',
+				);
+				break;
+			case 06:
+				return array(
+					'code' => 06,
+					'msg' => 'Token has expired',
+				);
+				break;
+			case 07:
+				return array(
+					'code' => 06,
+					'msg' => 'Token vali expired',
+				);
 				break;
 		}
 	}
