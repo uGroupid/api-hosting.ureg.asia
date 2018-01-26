d9acedeaa210c98f711348add2fdb132d53ee279
uGroupid <ugroupid>
2018-01-26 17:02:58 +0700

    edit config mongodb connect

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('redis','mongo_db','session','encrypt');
+$autoload['libraries'] = array('JWT','redis','mongo_db','session','encrypt');
 
 
 /*
--- application/controllers/testcase.php
+++ application/controllers/testcase.php
@@ -3,14 +3,66 @@ require APPPATH .'/libraries/ISPConfig_Controller.php';
 class Testcase extends ISPConfig_Controller {
 	function __construct(){
 		parent::__construct();
-		
+		$this->consumer_key = CONSUMER_KEY();
+		$this->consumer_secret = CONSUMER_SECRET();
+		$this->consumer_ttl = CONSUMER_TTL();
 	}
 	
+	
+	
+	
 	public function index(){
-	 
-	 echo "echo hello";
-		
+		$uid = "123";
+		$param = array(
+			'full_name' => 'handesk',
+			'age' => 26,
+			'addr' => '132/62 Cầu Diễn - Minh Khai - Quận Bắc Từ Liêm - Hà Nội ',
+			'phone' => '093-233-7122',
+			'auth',
+		);
+		$param_json = json_encode($param,true);
+		$token = $this->Initialize_Token($uid,$param_json);
+		echo $token;
 	}
+	
+	
+	 
+	private function validate($token)
+    {
+        try {
+            $decodeToken = $this->jwt->decode($token, $this->consumer_secret);
+            $ttl_time = strtotime($decodeToken->issuedAt);
+            $now_time = strtotime(date(DATE_ISO8601, strtotime("now")));
+            if(($now_time - $ttl_time) > $decodeToken->ttl) {
+				 return false;
+            } else {
+                return true;
+            }
+        } catch (Exception $e) {
+            return false;
+        }
+    }
+	private function decode($token)
+    {
+        try{
+            $decodeToken = $this->jwt->decode($token, $this->consumer_secret);
+            return $decodeToken;
+        }catch (Exception $e) {
+            return false;
+        }
+    }
+	private function Initialize_Token($uid,$param){
+       $token = $this->jwt->encode(array(
+            'consumer_key' => $this->consumer_secret,
+            'uid' => $uid,
+            'param' => $param,
+            'issuedAt' => date(DATE_ISO8601, strtotime("now")),
+            'ttl' => $this->consumer_ttl,
+        ), $this->consumer_secret);
+        return $token;
+	}
+		
+	
 }
 
 ?>
