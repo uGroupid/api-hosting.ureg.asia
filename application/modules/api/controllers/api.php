033bb39b9fd0757e487fcb40287028707745a994
uGroupid <ugroupid>
2018-01-26 19:04:01 +0700

    edit api

--- application/modules/api/controllers/api.php
+++ application/modules/api/controllers/api.php
@@ -22,6 +22,38 @@ class Api extends REST_Controller {
 				api_message(10), 
 				api_message(11), 
 				api_message(12), 
+				array(
+				  'code' => '20-29',
+				  'msg' => 'Error FTP Syntax',
+				 ),
+				array(
+				  'code' => '30-39',
+				  'msg' => 'Error DATABASE Syntax',
+				 ),
+				array(
+				  'code' => '40-49',
+				  'msg' => 'Error Website Syntax',
+				 ),
+				 array(
+				  'code' => '50-59',
+				  'msg' => 'Error Clients Syntax',
+				 ),
+				 array(
+				  'code' => '60-69',
+				  'msg' => 'Error Email Syntax',
+				 ),
+				  array(
+				  'code' => '70-79',
+				  'msg' => 'Error Disk Syntax',
+				 ),
+				  array(
+				  'code' => '80-89',
+				  'msg' => 'Error Payment Syntax',
+				 ),
+				 array(
+				  'code' => '90-98',
+				  'msg' => 'Monitor Server Syntax',
+				 ),
 			);
 		$this->response($response);
 	}
