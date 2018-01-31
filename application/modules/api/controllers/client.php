d335167f259b3a9f8d47502ff3c58813b0cccb4e
uGroupid <ugroupid>
2018-01-31 17:00:50 +0700

    add source

--- application/modules/api/controllers/client.php
+++ application/modules/api/controllers/client.php
@@ -1,9 +1,37 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 require APPPATH . '/libraries/REST_Controller.php';
 class Client extends REST_Controller {
-	function __construct(){
+	public $token;
+	public $username;
+	public $passwords;
+	public $email;
+	/////////////////////////////////
+	public $street;
+	public $zip;
+	public $city;
+	public $state;
+	public $country;
+	public $telephone;
+	public $mobile;
+	public $fax;
+	//////////////////////////////////
+	public $company_name;
+	public $vat_id;
+	public $customer_no;
+	public $contact_name;
+	public $language;
+	public $usertheme;
+	public $notes;
+	public $created_at;
+	//////////////////////////////////
+	
+	function __construct($token){
 		parent::__construct();
-		
+		$this->token = $token;
+		if(!$this->token){
+			$response = array('msg' => api_message(05),);
+			$this->response($response);
+		}
 	}
 	public function index_get(){
 		$response = array('');
